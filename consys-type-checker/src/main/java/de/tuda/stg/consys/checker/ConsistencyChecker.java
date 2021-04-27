8f94966be24dfa82273bda81c1ff7b98314e3420
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2021-04-27 17:04:28 +0200

    Fix for upstream changes

--- consys-type-checker/src/main/java/de/tuda/stg/consys/checker/ConsistencyChecker.java
+++ consys-type-checker/src/main/java/de/tuda/stg/consys/checker/ConsistencyChecker.java
@@ -1,14 +1,16 @@
 package de.tuda.stg.consys.checker;
 
+import org.checkerframework.checker.compilermsgs.qual.CompilerMessageKey;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.framework.source.SupportedLintOptions;
 import org.checkerframework.framework.source.SuppressWarningsKeys;
+import org.checkerframework.framework.source.SuppressWarningsPrefix;
 
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-@SupportedLintOptions({"disableSubChecker"})
+
 @SuppressWarningsKeys({"consistency"})
 public class ConsistencyChecker extends BaseTypeChecker {
 
@@ -18,13 +20,19 @@ public class ConsistencyChecker extends BaseTypeChecker {
 
     @Override
     protected LinkedHashSet<Class<? extends BaseTypeChecker>> getImmediateSubcheckerClasses() {
-        //if (getLintOption("disableSubChecker", false))
-        //    return new LinkedHashSet<>();
+        var checkers = super.getImmediateSubcheckerClasses();
+        checkers.add(SubConsistencyChecker.WeakSubConsistencyChecker.class);
+        checkers.add(SubConsistencyChecker.StrongSubConsistencyChecker.class);
+        return checkers;
+    }
 
-        //LinkedHashSet<Class<? extends BaseTypeChecker>> checkers = super.getImmediateSubcheckerClasses();
-        //checkers.add(SubConsistencyChecker.WeakSubConsistencyChecker.class);
-        //checkers.add(SubConsistencyChecker.StrongSubConsistencyChecker.class);
-        //return checkers;
-        return super.getImmediateSubcheckerClasses();
+    @Override
+    public void reportError(Object source, @CompilerMessageKey String messageKey, Object... args) {
+        super.reportError(source, messageKey, args);
+    }
+
+    @Override
+    public void reportWarning(Object source, @CompilerMessageKey String messageKey, Object... args) {
+        super.reportWarning(source, messageKey, args);
     }
 }
